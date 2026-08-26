fe379c239d96c4fa267596b198ef36fb241f5f5a
pranav shinde <pranavshinde0347@gmail.com>
2026-08-26 11:30:57 +0530

    added python string operations

---
+++ string_functions.py
@@ -0,0 +1,9 @@
+name=input("enter your full name : ")
+#string functions
+print(name.capitalize())
+print(name.count("o"))
+print(name.find("r"))
+print(name.find("h"))
+print(name.upper())
+print(name.replace("p","s"))
+print(name.lower())
