d3e2b95125ea63a95c179710ccaffbcfc739fbc0
Pranav Shinde <pranavshinde0347@gmail.com>
2026-09-02 20:11:44 +0530

    practiced while loop ,for loops questions.

---
+++ loops.py
@@ -0,0 +1,113 @@
+
+# 1
+
+i=1
+while i<=100:
+    print(i)
+    i=i+1
+    
+#2
+
+# i=100
+# while i>=1:
+#     print(i)
+#     i=i-1
+    
+# #3
+
+# n=int(input("entrer a number : "))
+
+# i=1
+# while i<=10:
+#     print(i*n) 
+#     i=i+1
+    
+#4
+
+# list=[1,4,9,16,25,36,49,64,81,100]
+# i=0
+# while i<=9:
+#     print(list[i])
+#     i=i+1
+
+#5
+
+# list=[1,4,9,16,25,36,49,64,81,100]
+# x=int(input("enter a number :"))
+
+# i=0
+# found=False
+# while i<len(list):
+#     if list[i]==x:
+#         found=True
+#         break
+        
+#     i=i+1
+    
+    
+# if found==True:
+#     print("found at index ",i)
+    
+# else:
+#     print("the number not found")
+ 
+ 
+#6
+
+# list=[10,20,36,80,45,"ps","ss"]
+
+# for i in list:
+#     print(i)
+
+
+#7
+
+# list=[10,20,36,80,45,"ps","ss"]
+
+# for i in range(0,len(list)):
+#     if i==3:
+#         break
+#     print(list[i])
+
+
+#8
+
+# list=[10,20,36,80,45,"ps","ss"]
+
+# for i in range(0,len(list)):
+#     if i==3:
+#         continue
+#     print(list[i])
+
+
+#9
+
+# num=[10,20,36,80,45,52,88,98,45]
+# for i in range(0,len(num)):
+#     pass
+    
+# print("hello pranav")
+
+
+#10
+
+# list=[10,20,36,80,45,"ps","ss"]
+
+# for i in list:
+#     print(i)
+    
+# else:
+#     print("loop end")
+    
+
+#11
+
+
+# list=[10,20,36,80,45,"ps","ss"]
+# for i in range(0,len(list)):
+#     if i==3:
+#         break
+#     print(list[i])
+
+# else:
+#     print("loop end") #this will execute only when for loop terminates without break
