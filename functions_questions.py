e1acf64912b4df155545d5170bd50b93e71e1bba
Pranav Shinde <pranavshinde0347@gmail.com>
2026-09-04 19:12:37 +0530

    practiced questions on functions

---
+++ functions_questions.py
@@ -0,0 +1,48 @@
+# program1 WAF to print the length of the list ,list is argument
+
+# list=[10,20,30,40,506,1]
+
+# def cal_len(list):
+#     print(len(list))
+
+# cal_len(list)
+
+
+    
+# program 2 WAF to print the elements of a list in a single line
+
+# movies=["got","uri","3 idiots","chhichhore"]
+
+# def print_ele(movies):
+#     for item in movies:
+#         print(item,end=" ")
+        
+# print_ele(movies)
+
+
+
+# program 3 WAf to calculate factorial of n , n is the parameter
+
+
+# n=4
+
+# def cal_fact(n,fact=1):
+#     for i in range(1,n+1):
+#         fact*=i
+#     print("factorial : ",fact)
+
+# cal_fact(n)
+
+
+# program 4 WAF to convert USD to INR
+
+
+
+# def convert_to_INR(usd):
+#     INR=usd*83
+#     print(INR,"inr")
+    
+# convert_to_INR(100)
+    
+
+
