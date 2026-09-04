f07bd1956c40fd96d72b4aa8c5d0288573575033
Pranav Shinde <pranavshinde0347@gmail.com>
2026-09-04 19:11:46 +0530

    learned functions concept in Python

---
+++ functions.py
@@ -0,0 +1,53 @@
+# program1
+# #function definition
+# def cal_sum(a,b):
+    
+#     return a+b
+
+
+# a=5
+# b=4
+# c=cal_sum(a,b)
+# print(c)
+
+
+
+# program 2
+
+# def hellol_fun():
+#     print("hello Pranav")
+    
+# hellol_fun()
+
+
+# program3
+
+# a=10
+# b=10
+# c=30
+# def avg(a,b,c):
+#     mean=(a+b+c)/3
+#     return mean
+
+# mean=avg(a,b,c)
+# print(mean)
+
+
+# program4
+
+# print("apna college",end=" ")
+# print("apna college")
+
+
+# program4  default args
+
+# def cal_mul(a=2,b=8):
+#     return a*b
+
+# # product=cal_mul()
+# # print(product)
+
+
+
+
+
