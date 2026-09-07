0ec8d5331bcbcc597909a74950297e01abbb9f19
Pranav Shinde <pranavshinde0347@gmail.com>
2026-09-07 18:57:14 +0530

    leaned oop concepts

---
+++ oops.py
@@ -0,0 +1,94 @@
+#                           program 1
+
+class Student :
+    name="Pranav"
+    
+s1=Student()
+print(s1.name)
+
+s2=Student()
+print(s2.name)
+
+
+#                           program 2
+
+# class Car:
+#     colour="blue"
+#     brand="Mercedes"
+    
+# c1=Car()
+# print(c1.colour)
+# print(c1.brand)
+
+#                           program 3
+
+# class Student:
+#     name="Pranav"
+#     def __init__(self):
+#         print("constructor called ")
+        
+# s1=Student()
+
+#                           program 4
+
+# class Student:
+    
+#     def __init__(self,name):
+#         self.name=name
+#         print("constructor called ")
+        
+# s1=Student("pranav")
+# print(s1.name)
+
+# s2=Student("Satyaa")
+# print(s2.name)
+
+#                           program 5
+
+# class Student:
+    
+#     def __init__(self,name,marks):#parameterized constructor
+#         self.name=name
+#         self.marks=marks
+#         print("constructor called ")
+        
+# s1=Student("pranav",80)
+# print(s1.name,s1.marks)
+
+# s2=Student("Satyaa",90)
+# print(s2.name,s2.marks)
+
+
+#                           program 6
+
+# class Student:
+#     college="trinity"
+#     def __init__(self,name,marks):#parameterized constructor
+#         self.name=name
+#         self.marks=marks
+#         print("constructor called ")
+        
+# s1=Student("pranav",80)
+# print(s1.name,s1.marks)
+
+# s2=Student("Satyaa",90)
+# print(s2.name,s2.marks)
+
+# print(s1.college)
+# print(s2.college)
+# print(Student.college)
+
+
+#                   program 7 METHODS
+
+
+# class Student:
+#     def __init__(self,name):
+#         self.name=name
+        
+#     def welcome(self):
+#         print("welcome",self.name)
+        
+        
+# s1=Student("pranav")
+# s1.welcome()
